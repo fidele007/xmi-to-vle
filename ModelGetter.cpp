930b7d6a6a458f2533b90006e0cffbab79532d09
fidele007 <forcefidele@gmail.com>
2016-08-12 11:49:14 +0700

    Implement functions to get model index knowing its idRef

--- ModelGetter.cpp
+++ ModelGetter.cpp
@@ -2,17 +2,41 @@
 #include "ModelGetter.hpp"
 #include <algorithm>
 #include <boost/bind.hpp>
+#include <boost/foreach.hpp>
 
-bool isModelID(const std::string& id, const Model& model) {
+using namespace std;
+
+bool isModelID(const string& id, const Model& model) 
+{
     return model.id == id;
 }
 
-int getModelIndexFromID(std::vector<Model> submodels, const std::string id) {
-    std::vector<Model>::iterator it = find_if(submodels.begin(), submodels.end(),
-                        boost::bind(&isModelID, id, _1));
-    if (it != submodels.end()) {
-        return std::distance(submodels.begin(), it);
+int getModelIndexFromID(vector<Model> submodels, const string id)
+{
+    vector<Model>::iterator it = find_if(submodels.begin(), submodels.end(),
+                                         boost::bind(&isModelID, id, _1));
+    if (it != submodels.end())
+        return distance(submodels.begin(), it);
+
+    return -1;
+}
+
+bool isModelIDRef(const string& idRef, const Model& model) 
+{
+    BOOST_FOREACH(const string& id, model.idRef) {
+        if (id == idRef)
+            return true;
     }
+
+    return false;
+}
+
+int getModelIndexFromIDRef(vector<Model> submodels, const string idRef) 
+{
+    vector<Model>::iterator it = find_if(submodels.begin(), submodels.end(),
+                                         boost::bind(&isModelIDRef, idRef, _1));
+    if (it != submodels.end())
+        return distance(submodels.begin(), it);
 
     return -1;
 }
--- ModelGetter.hpp
+++ ModelGetter.hpp
@@ -1,2 +1,4 @@
 bool isModelID(const std::string& id, const Model& model);
 int getModelIndexFromID(std::vector<Model> submodels, const std::string id);
+bool isModelIDRef(const std::string& id, const Model& model);
+int getModelIndexFromIDRef(std::vector<Model> submodels, const std::string idRef);
