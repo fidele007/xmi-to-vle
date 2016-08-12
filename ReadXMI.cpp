fbca7b38582ffa47cbb23a8e1f91339df33bea12
fidele007 <forcefidele@gmail.com>
2016-08-12 11:50:08 +0700

    Reimplement ReadXMI function for XMI generated by Modelio

--- ReadXMI.cpp
+++ ReadXMI.cpp
@@ -4,99 +4,103 @@
 #include <boost/property_tree/xml_parser.hpp>
 #include <boost/property_tree/ptree.hpp>
 #include <boost/foreach.hpp>
+#include <boost/algorithm/string.hpp>
+
+using namespace std;
+
+vector<string> split(string data, string token)
+{
+    vector<string> output;
+    size_t pos = string::npos;
+    do
+    {
+        pos = data.find(token);
+        output.push_back(data.substr(0, pos));
+        if (string::npos != pos)
+            data = data.substr(pos + token.size());
+    } while (string::npos != pos);
+
+    return output;
+}
 
 VLEProject read(std::istream& is)
 {
     using boost::property_tree::ptree;
+
     ptree pt;
     read_xml(is, pt);
 
     VLEProject vle_project;
     Model mainModel = vle_project.model;
+    
+    const ptree& root = pt.get_child("uml:Model");
+    mainModel.name = root.get<string>("<xmlattr>.name");
+    mainModel.type = MT_coupled;
 
-    BOOST_FOREACH(ptree::value_type const& v, pt.get_child("XMI")) {
-        if (v.first == "<xmlattr>") {
-            const ptree& root = pt.get_child("XMI");
-            vle_project.date = root.get<std::string>("<xmlattr>.timestamp");
-        } else if (v.first == "XMI.content") {
-            mainModel.name =
-                v.second.get<std::string>("UML:Model.<xmlattr>.name");
-        }
+    const ptree& eltTree = root.get_child("packagedElement");
+    string diagramType = eltTree.get<string>("<xmlattr>.xmi:type");
+    if (diagramType != "uml:Interaction") {
+        cout << "XMI is not of a sequence a diagram. Abort." << endl;
+        exit (EXIT_FAILURE);
     }
 
-    typedef ptree::path_type path;
-    using namespace std;
-    string eltPath = "XMI/XMI.content/UML:Model/UML:Namespace.ownedElement";
-    const ptree& eltTree = pt.get_child(path(eltPath, '/'));
+    BOOST_FOREACH(ptree::value_type const& child, eltTree) {
+        if (child.first == "lifeline") {
+            Model submodel;
+            string fullModelName = child.second.get<string>("<xmlattr>.name");
+            size_t colonPos = fullModelName.find(":");
+            if (colonPos == string::npos)
+                continue;
 
-    BOOST_FOREACH(ptree::value_type const& elt, eltTree) {
-        if (elt.first == "UML:Collaboration") {
-            // in sequence or collaboration diagram
-            const ptree& roleTree =
-                elt.second.get_child(path("UML:Namespace.ownedElement", '/'));
+            string modelType = fullModelName.substr(0, colonPos);
+            if (boost::iequals(modelType, "atomic"))
+                submodel.type = MT_atomic;
+            else if (boost::iequals(modelType, "coupled"))
+                submodel.type = MT_coupled; //TODO: Go to another XMI
+            else
+                continue;
 
-            // Get submodels
-            BOOST_FOREACH(ptree::value_type const& role, roleTree) {
-                if (role.first == "UML:ClassifierRole") {
-                    string idPath = "<xmlattr>/xmi.id";
-                    Model submodel;
-                    submodel.name = role.second.get("<xmlattr>.name", "");
-                    submodel.id = role.second.get(path(idPath, '/'), "");
-                    mainModel.submodels.push_back(submodel);
-                }
+            submodel.name = fullModelName.substr(colonPos + 1);
+            submodel.id = child.second.get<string>("<xmlattr>.xmi:id");
+            string idRef = child.second.get<string>("<xmlattr>.coveredBy");
+            submodel.idRef = split(idRef, " ");
+
+            mainModel.submodels.push_back(submodel);
+        } else if (child.first == "message") {
+            Connection con;
+            con.name = child.second.get("<xmlattr>.name", "");
+            con.type = CT_internal;
+
+            string origID = child.second.get<string>("<xmlattr>.sendEvent");
+            int origIndex = getModelIndexFromIDRef(mainModel.submodels, origID);
+            if (origIndex == -1) {
+                cout << "Model with IDRef " << origID << " not found" << endl;
+                continue;
+            }
+            Model origModel = mainModel.submodels[origIndex];
+            con.origin.modelName = origModel.name;
+            con.origin.portName = con.name + ".out";
+            Port outPort;
+            outPort.name = con.origin.portName;
+            origModel.outPorts.push_back(outPort);
+            mainModel.submodels[origIndex] = origModel;
+
+            string destID = child.second.get<string>("<xmlattr>.receiveEvent");
+            int destIndex = getModelIndexFromIDRef(mainModel.submodels, destID);
+            if (destIndex == -1) {
+                cout << "Model with IDRef " << destID << " not found" << endl;
+                continue;
             }
 
-            // Get messages and connections
-            string mesPath = "UML:Collaboration.interaction/UML:Interaction/"
-                             "UML:Interaction.message";
-            const ptree& mesTree =
-                elt.second.get_child(path(mesPath, '/'));
-            Model submodels;
-            BOOST_FOREACH(ptree::value_type const& mes, mesTree) {
-                if (mes.first == "UML:Message") {
-                    Connection con;
-                    con.name = mes.second.get("<xmlattr>.name", "");
-                    con.type = CT_internal;
+            Model destModel = mainModel.submodels[destIndex];
+            con.destination.modelName = destModel.name;
+            con.destination.portName = con.name + ".in";
+            Port inPort;
+            inPort.name = con.destination.portName;
+            destModel.inPorts.push_back(inPort);
+            mainModel.submodels[destIndex] = destModel;
 
-                    string senderPath = "UML:Message.sender/UML:ClassifierRole/"
-                                        "<xmlattr>/xmi.idref";
-                    string origID = mes.second.get(path(senderPath, '/'), "");
-                    int origIndex = getModelIndexFromID(mainModel.submodels, 
-                                                        origID);
-                    if (origIndex == -1) {
-                        cout << "Model ID " << origID << " not found" << endl;
-                        break;
-                    }
-
-                    Model origModel = mainModel.submodels[origIndex];
-                    con.origin.modelName = origModel.name;
-                    con.origin.portName = con.name + ".out";
-                    Port outPort;
-                    outPort.name = con.origin.portName;
-                    origModel.outPorts.push_back(outPort);
-                    mainModel.submodels[origIndex] = origModel;
-
-                    string rcvPath = "UML:Message.receiver/UML:ClassifierRole/"
-                                     "<xmlattr>/xmi.idref";
-                    string destID = mes.second.get(path(rcvPath, '/'), "");
-                    int destIndex = getModelIndexFromID(mainModel.submodels, 
-                                                        destID);
-                    if (destIndex == -1) {
-                        cout << "Model ID " << destID << " not found" << endl;
-                        break;
-                    }
-
-                    Model destModel = mainModel.submodels[destIndex];
-                    con.destination.modelName = destModel.name;
-                    con.destination.portName = con.name + ".in";
-                    Port inPort;
-                    inPort.name = con.destination.portName;
-                    destModel.inPorts.push_back(inPort);
-                    mainModel.submodels[destIndex] = destModel;
-
-                    mainModel.connections.push_back(con);
-                }
-            }
+            mainModel.connections.push_back(con);
         }
     }
 
